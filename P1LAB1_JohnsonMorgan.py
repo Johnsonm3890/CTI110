3fde6ad00e7d733b6a4a865290d573f28a36680d
Johnsonm3890 <johnsonm3890@student.faytechcc.edu>
2025-02-23 17:14:32 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_JohnsonMorgan.py
@@ -0,0 +1,13 @@
+# Morgan Johnson
+# 2/22/25
+# P1LAB1
+# Using Input and print functions to create a program that allows students to add their first and last name.
+
+print('Enter your first name:', end=' ')
+first_name=input()
+print('Enter your last name:', end=' ')
+last_name=input()
+print('Hello,', end=" ")
+print(first_name, end=" ")
+print(last_name, end="")
+print("! Welcome to CTI-110")
